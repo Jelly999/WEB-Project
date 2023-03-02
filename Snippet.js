621d2533ae2ac4ba27db5bf853dab26c286d55a8
Jelly999 <aalto98@outlook.com>
2023-03-02 19:41:12 +0200

    Comments

--- Snippet.js
+++ Snippet.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+//Schema for Code snippets with email address and code snippet posts are in items array
+
 let snippetSchema = new Schema({
     user: String,
     items: Array,
